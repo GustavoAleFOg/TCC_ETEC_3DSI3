2ef5a59752548a4263ab6d81b162df95ed261392
ShopinUnline <shopinunline@gmail.com>
2025-09-08 17:28:00 -0300

    alpha_versao_0.0.1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Header from './Componentes/Header/Header'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <div className=''>
+        <Header />
+        
+
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componentes/Header/Header.jsx
@@ -0,0 +1,12 @@
+function Header () {
+    return(
+
+        <>
+            <div className="">
+                <h1>teste</h1>
+            </div>
+        </>
+    )
+}
+
+export default Header
